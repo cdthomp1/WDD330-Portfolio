573e946139aadddd7aecbb9be1e9feed38eed3fe
cdthomp1 <tho17015@byui.edu>
2020-07-24 20:55:30 -0600

    social food

---
+++ week14/routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+
+
+
+// Welcome Page
+router.get('/dasboard', forwardAuthenticated, (req, res) => res.render('unauth'));
+
+router.get('/', (req, res) => res.render('index', {user: req.user }));
+router.get('/public', (req, res) => res.render('publicRecs', {user: req.user }));
+router.get('/about', (req, res) => res.render('construction', {user: req.user }));
+router.get('/addRec', ensureAuthenticated, (req, res) => res.render('addRec', {user: req.user }));
+
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+  res.render('dashboard', { user: req.user, auth: true });
+});
+
+module.exports = router;
